d012a5a925cd7e14b6e57f4511d3c78442e69e6a
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2023-09-12 13:42:35 +0600

    even odd project bug fix and comparison project add

--- comparison_tool.php
+++ comparison_tool.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <title>Even or Odd Checker</title>
+    <title>Comparison Tool</title>
     <style>
 
     </style>
@@ -15,19 +15,12 @@
     $result = null;
     if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
-      $number = $_POST['number'];
-      $lastNumber = $number % 2;
-      if ($lastNumber == 0)
-      {
-        $result = 'Even';
-      }
-      else
-      {
-        $result = 'Odd';
-      }
-      
 
-          
+        $first_number = $_POST['first_number'];
+        $second_number = $_POST['second_number'];
+
+        $first_number > $second_number ?  $result = "{$first_number} is the larger number" :  $result = "{$second_number} is the larger number";
+      
     }
   ?>
 
@@ -35,24 +28,25 @@
         <div class="mt-5 mx-auto" style="width: 500px;;">
             <div class="card">
                 <div class="card-header">
-                Even or Odd Checker
+                Comparison Tool
                 </div>
                 <form action="#" method="post" action="">
                     <div class="card-body">
                         <strong><p>Result: <span>
                           <?php
-                          echo $result != null ?  "Tha number  is " : ' ';
-                          echo $number ?? ' ';
-                          echo ' '.$result;
+                          echo  $result;
                           ?>
-
                         </span></p></strong>
                        <div class="my-3">
-                        <label for="number" class="form-label">Insert Number *</label>
-                        <input type="number" id="number" name="number" class="form-control" aria-describedby="number" required>
+                        <label for="first_number" class="form-label">First Number*</label>
+                        <input type="first_number" id="first_number" name="first_number" class="form-control" aria-describedby="first_number" required>
+                       </div>
+                       <div class="my-3">
+                        <label for="second_number" class="form-label">Second Number*</label>
+                        <input type="second_number" id="second_number" name="second_number" class="form-control" aria-describedby="second_number" required>
                        </div>
                        <div class="d-grid mt-4">
-                        <button class="btn btn-primary" type="submit">Check Number Type</button>
+                        <button class="btn btn-primary" type="submit">Check Larger  Number</button>
                       </div>
                     </div>
                     
